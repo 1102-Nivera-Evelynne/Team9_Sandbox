6aad424619f286769597f3c781c263b3801cfa2c
Kierian00 <Kierianeran@gmail.com>
2025-04-27 15:49:25 -0700

    Color Mapping Test

---
+++ Team9Sandbox/Kierian Environment/Assets/Scene2Script.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class MainMenu2 : MonoBehaviour
+{
+    public void PlayGame2()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+}
+
