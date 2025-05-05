0c03e090006e8994fa617a14205a36ba09feea06
1102-Nivera-Evelynne <enivera1@gmail.com>
2025-05-04 19:13:54 -0700

    Add environment menu

--- Team9Sandbox/Assets/Scripts/MainMenu.cs
+++ Team9Sandbox/Assets/Scripts/MainMenu.cs
@@ -3,11 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public GameObject menu;
+    public GameObject menu, envMenu;
 
     public void StartGame()
     {
         SceneManager.LoadScene(1);
         SceneManager.UnloadSceneAsync(0);
+    }
+
+    public void ShowEnvMenu()
+    {
+        menu.SetActive(false);
+        envMenu.SetActive(true);
     }
 }
