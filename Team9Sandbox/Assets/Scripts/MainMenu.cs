bedc22789dd067fe1343aa027854ede11dec79d0
1102-Nivera-Evelynne <enivera1@gmail.com>
2025-05-07 12:30:31 -0700

    menu ui update

--- Team9Sandbox/Assets/Scripts/MainMenu.cs
+++ Team9Sandbox/Assets/Scripts/MainMenu.cs
@@ -17,10 +17,23 @@ public class MainMenu : MonoBehaviour
         envMenu.SetActive(true);
     }
 
+    public void showMainMenu()
+    {
+        menu.SetActive(true);
+        envMenu.SetActive(false);
+    }
+
     public void StartGameDark()
     {
         //Uncomment these when scene is added
        // SceneManager.LoadScene(2);
        // SceneManager.UnloadSceneAsync(0);
+    }
+
+    public void startTutorial()
+    {
+        //Uncomment these when scene is added
+        // SceneManager.LoadScene(3);
+        // SceneManager.UnloadSceneAsync(0);
     }
 }
